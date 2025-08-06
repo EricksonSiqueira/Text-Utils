d4a2539764812e05ecd639308a12265d4f44f45b
Erickson <ericksonsiqueirasilva@gmail.com>
2025-08-06 01:24:14 -0300

    feat: creat utils functions joinTextIntoTabs and joinTextIntoLines

---
+++ utils/spreadsheet.ts
@@ -0,0 +1,7 @@
+export const joinTextIntoTabs = (arrayOfText: string[]) => {
+  return arrayOfText.join("\t")
+}
+
+export const joinTextIntoLines = (arrayOfText: string[]) => {
+  return arrayOfText.join("\n")
+}
